d8edc6301fc8731767dc1846324318cd528e9363
fangbrian <fangbrian@gmail.com>
2015-07-08 11:34:49 -0700

    Added game board view

--- app/src/main/java/gameui/android/ggl/com/tictactoe/GameBoard.java
+++ app/src/main/java/gameui/android/ggl/com/tictactoe/GameBoard.java
@@ -2,19 +2,47 @@ package gameui.android.ggl.com.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by fangbrian on 7/8/15.
  */
 public class GameBoard extends Activity {
+    private GridView mGridView;
+    private final String[] mBlocks = new String[] {
+            "*", "*", "*",
+            "*", "*", "*",
+            "*", "*", "*"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_board);
 
+        mGridView = (GridView) findViewById(R.id.gridview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mBlocks);
+
+        mGridView.setAdapter(adapter);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 
